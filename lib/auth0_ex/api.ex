d37bab826c75532fa41c19dc2860aa4c453f7e42
techgaun <coolsamar207@gmail.com>
2017-01-28 11:41:56 -0600

    fix 1.4 warnings

--- lib/auth0_ex/api.ex
+++ lib/auth0_ex/api.ex
@@ -8,38 +8,50 @@ defmodule Auth0Ex.Api do
       alias Auth0Ex.Parser
       import Auth0Ex.Utils
 
+      @doc false
       def build_url(path, params) do
-        "#{base_url}#{path}?#{URI.encode_query(params)}"
+        "#{base_url()}#{path}?#{URI.encode_query(params)}"
       end
 
+      @doc false
       def do_get(path, params) when is_map(params) do
         do_request(:get, path, params, "")
       end
 
+      @doc false
       def do_get(path, params) when is_list(params) do
         do_get(path, Enum.into(params, %{}))
       end
 
+      @doc false
       def do_post(path, body \\ %{})
+
+      @doc false
       def do_post(path, body) when is_list(body) do
         do_post(path, Enum.into(body, %{}))
       end
+
+      @doc false
       def do_post(path, body) do
         do_request(:post, path, %{}, Poison.encode!(body))
       end
 
+      @doc false
       def do_put(path, body \\ %{}) do
         do_request(:put, path, %{}, Poison.encode!(body))
       end
 
+      @doc false
       def do_patch(path, body \\ %{}) do
         do_request(:patch, path, %{}, Poison.encode!(body))
       end
 
+      @doc false
       def do_delete(path) do
         do_request(:delete, path)
       end
 
+      @doc false
       def do_delete(path, params) do
         do_request(:delete, path, params)
       end
@@ -47,8 +59,8 @@ defmodule Auth0Ex.Api do
       defp do_request(method, path, params \\ %{}, req_body \\ "") do
         uri = build_url(path, params)
         method
-        |> HTTPoison.request(uri, req_body, req_header, http_opts)
-        |> Parser.parse
+        |> HTTPoison.request(uri, req_body, req_header(), http_opts())
+        |> Parser.parse()
       end
     end
   end
--- lib/auth0_ex/utils.ex
+++ lib/auth0_ex/utils.ex
@@ -3,12 +3,12 @@ defmodule Auth0Ex.Utils do
   Collection module of various utils needed for Auth0Ex
   """
 
-  def base_url, do: "https://#{domain}.auth0.com/api/v2/"
+  def base_url, do: "https://#{domain()}.auth0.com/api/v2/"
   def domain, do: Application.get_env(:auth0_ex, :domain)
   def mgmt_token, do: Application.get_env(:auth0_ex, :mgmt_token)
   def http_opts, do: Application.get_env(:auth0_ex, :http_opts) || []
   def ua, do: Application.get_env(:auth0_ex, :user_agent) || "Auth0Ex <https://github.com/techgaun/auth0_ex>"
-  def req_header, do: [{"User-Agent", ua},
-                       {"Authorization", "Bearer #{mgmt_token}"},
+  def req_header, do: [{"User-Agent", ua()},
+                       {"Authorization", "Bearer #{mgmt_token()}"},
                        {"Content-Type", "application/json"}]
 end
