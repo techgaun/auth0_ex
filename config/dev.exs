bac9e1fa5a32f53695d540e51d8fc02f504e233d
techgaun <coolsamar207@gmail.com>
2017-02-09 16:00:44 -0600

    start integrating token generation for auth0 management client

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,6 @@ use Mix.Config
 config :auth0_ex,
   domain: System.get_env("AUTH0_DOMAIN"),
   mgmt_token: System.get_env("AUTH0_MGMT_TOKEN"),
+  mgmt_client_id: System.get_env("AUTH0_MGMT_CLIENT_ID"),
+  mgmt_client_secret: System.get_env("AUTH0_MGMT_CLIENT_SECRET"),
   http_opts: []
--- lib/auth0_ex/utils.ex
+++ lib/auth0_ex/utils.ex
@@ -2,13 +2,24 @@ defmodule Auth0Ex.Utils do
   @moduledoc """
   Collection module of various utils needed for Auth0Ex
   """
-
   def base_url, do: "https://#{domain()}.auth0.com/api/v2/"
-  def domain, do: Application.get_env(:auth0_ex, :domain)
-  def mgmt_token, do: Application.get_env(:auth0_ex, :mgmt_token)
-  def http_opts, do: Application.get_env(:auth0_ex, :http_opts) || []
-  def ua, do: Application.get_env(:auth0_ex, :user_agent) || "Auth0Ex <https://github.com/techgaun/auth0_ex>"
+  def domain, do: get_config(:domain)
+
+  def mgmt_token do
+    case get_config(:mgmt_token) do
+      token when is_binary(token) -> token
+
+      _ ->
+        cient_id = get_config(:mgmt_client_id)
+        client_secret = get_config(:mgmt_client_secret)
+    end
+  end
+
+  def http_opts, do: get_config(:http_opts) || []
+  def ua, do: get_config(:user_agent) || "Auth0Ex <https://github.com/techgaun/auth0_ex>"
   def req_header, do: [{"User-Agent", ua()},
                        {"Authorization", "Bearer #{mgmt_token()}"},
                        {"Content-Type", "application/json"}]
+
+  defp get_config(key), do: Application.get_env(:auth0_ex, key)
 end
