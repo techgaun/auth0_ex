12910792689a9228e7e43bc05360264ceb8e7557
techgaun <coolsamar207@gmail.com>
2017-02-20 04:11:46 -0600

    bump version for hex release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Auth0Ex.Mixfile do
 
   def project do
     [app: :auth0_ex,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
