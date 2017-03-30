97b8c9ad7eb3ee2814838bb67cdbdc69f2e68b5f
techgaun <coolsamar207@gmail.com>
2017-03-30 15:37:14 -0500

    bump version and documentations

--- lib/auth0_ex/management/ticket.ex
+++ lib/auth0_ex/management/ticket.ex
@@ -6,10 +6,20 @@ defmodule Auth0Ex.Management.Ticket do
   use Auth0Ex.Api, for: :mgmt
   @path "tickets"
 
+  @doc """
+  Create an email verification ticket
+
+      iex> Auth0Ex.Management.Ticket.email_verification("auth0|user_id", %{result_url: "http://myapp.com/callback", ttl_sec: 0})
+  """
   def email_verification(user_id, body) do
     do_post("#{@path}/email-verification", Map.put(body, :user_id, user_id))
   end
 
+  @doc """
+  Create a password change ticket
+
+      iex> Auth0Ex.Management.Ticket.password_change(%{result_url: "http://myapp.com/callback"})
+  """
   def password_change(body) do
     do_post("#{@path}/password-change", body)
   end
